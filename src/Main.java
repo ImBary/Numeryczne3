b0c302046c7684d16f20b490f1417239375714bb
ImBary <bartosz.sm@outlook.com>
2024-05-14 14:52:35 +0200

    s1/s2/s3/s4 tests

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,23 @@ public class Main {
 
         //MetodaProstokatow mp = new MetodaProstokatow();
         //mp.testPolaKola();
+       
+        trapezTesty();
+
+
+
+    }
+
+
+
+    public static void trapezTesty(){
         MetodaTrapezow s = new MetodaTrapezow(-1, 1, 10000);
         DoubleUnaryOperator circleFunction = x -> Math.sqrt(1 - x * x);
         System.out.println("oczekiwane "+ Math.PI/2 +", wynik: "+s.oblicz(circleFunction));//1.5707946637152863
 
         MetodaTrapezow s2 = new MetodaTrapezow(0, 1, 10000);
         DoubleUnaryOperator UFunction = x -> x * x;
-        System.out.println("oczekiwane 1 wynik: " + s2.oblicz(UFunction)); // 0.333
+        System.out.println("oczekiwane 1/3 wynik: " + s2.oblicz(UFunction)); // 0.333
 
 
 
@@ -23,10 +33,6 @@ public class Main {
         MetodaTrapezow s4 = new MetodaTrapezow(0, Math.PI, 10000);
         DoubleUnaryOperator sinFunction = x -> Math.sin(x);
         System.out.println("oczekiwane 2 wynik: " + s4.oblicz(sinFunction)); // 1.9999999835506608
-
-
-        
-
 
 
     }
